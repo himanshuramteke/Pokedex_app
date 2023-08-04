a2736aee5153b92602f1e5d253424e1127189549
Himanshu Ramteke <himanshuramteke007@gmail.com>
2023-08-04 22:56:56 +0530

    started creating pokedex app

---
+++ pokedex_app/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Search from './components/Search/Search';
+import Pokedex from './components/pokedex/pokedex'
+
+function App() {
+  
+  return (
+    <>
+      <Pokedex />
+    
+    </>
+  )
+}
+
+export default App;
